4db248d9eaca7fbb88446d8d28001b8d644b390e
danielpinoga <daniel.pino@generalassemb.ly>
2018-07-24 19:16:52 -0400

    removing semicolons

--- routes/unicorns.js
+++ routes/unicorns.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ key: 'value' });
-});
+  res.json({ key: 'value' })
+})
 
-module.exports = router;
+module.exports = router
