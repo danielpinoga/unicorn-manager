9916fcbf535115388385f98cad31e34c8d4eac64
danielpinoga <daniel.pino@generalassemb.ly>
2018-07-24 19:09:14 -0400

    removing default user routes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
